3fc359dae5f7f0a0fdbe980bb9ea2e48f1b781fe
smoyano <smoyano@slingr.io>
2024-06-24 13:52:27 -0300

    Hq crm table widget sample

---
+++ views/flex/hqCrmDashboardTableWidget.js
@@ -0,0 +1,81 @@
+
+function newMeetingsTableWidgetCalculation(record, options) {
+    // TODO: retrieve data
+    let leadsData = sys.data.aggregate('leads', []);
+    let leadsTotals = sys.data.aggregate('leads', []);
+
+    // build table
+
+    // build columns
+    let columns = [
+        {
+            label: 'Lead source',
+            name: 'leadSource'
+        }
+    ];
+    while (leadsData.hasNext()) {
+        let leadRecord = leadsData.next();
+        let periods = leadRecord.periods;
+        for (let periodIndex = 0; periodIndex < periods.length; periodIndex++) {
+            let existingColumn = columns.find(column => column.name === periods[periodIndex].name);
+            if (!existingColumn) {
+                columns.push({
+                    label: periods[periodIndex].label,
+                    name: periods[periodIndex].name,
+                    type: 'integer'
+                });
+            }
+        }
+    }
+    columns.push({
+        label: 'YTD',
+        name: 'ytd',
+        type: 'integer'
+    });
+    columns.push({
+        label: 'YTD vs TGT',
+        name: 'ytdVsTgt',
+        type: 'percentage',
+        options: {
+            style: {
+                fontColor: 'green'
+            }
+        }
+    });
+
+    // build rows
+    let rows = [];
+    while (leadsData.hasNext()) {
+        let leadRecord = leadsData.next();
+        let row = {
+            leadSource: leadRecord.name
+        }
+        let periods = leadRecord.periods;
+        for (let periodIndex = 0; periodIndex < periods.length; periodIndex++) {
+            row[periodIndex.name] = periods[periodIndex].total
+        }
+        row['ytd'] = leadRecord.ytd;
+        row['ytdVsTgt'] = leadRecord.ytdVsTgt;
+        rows.push(row);
+    }
+    let totalRow = {
+        leadSource: 'Grand Total',
+    };
+    let totalPeriods = leadsTotals.periods;
+    for (let periodIndex = 0; periodIndex < totalPeriods.length; periodIndex++) {
+        totalRow[periodIndex.name] = totalRow[periodIndex].total
+    }
+    rows.push({
+        cells: totalRow,
+        options: {
+            style: {
+                fontWeight: 'bold'
+            }
+        }
+    });
+
+    return {
+        columns: columns,
+        rows: rows
+    };
+}
