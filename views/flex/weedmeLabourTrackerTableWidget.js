230a60b05823f3636c62cd4ef88b3c4f6020ddc5
smoyano <smoyano@slingr.io>
2024-06-24 14:11:59 -0300

    Weedme table widget sample

---
+++ views/flex/weedmeLabourTrackerTableWidget.js
@@ -0,0 +1,67 @@
+
+function labourTrackerTableWidgetCalculation(record, options) {
+    // TODO: retrieve data
+    let labourTrackerData = {};
+
+    // build table
+
+    // build columns
+    let columns = [
+        {
+            label: 'Position',
+            name: 'position'
+        },
+        {
+            label: 'Active workers',
+            name: 'activeWorkers',
+            type: 'html',
+            options: {
+                style: {
+                    fontColor: 'green'
+                }
+            }
+        },
+        {
+            label: 'On Break workers',
+            name: 'onBreakWorkers',
+            type: 'html',
+            options: {
+                style: {
+                    fontColor: 'orange'
+                }
+            }
+        },
+        {
+            label: 'Idle workers',
+            name: 'idleWorkers',
+            type: 'html',
+            options: {
+                style: {
+                    fontColor: 'brown'
+                }
+            }
+        },
+        {
+            label: 'Total',
+            name: 'total',
+            type: 'integer'
+        }
+    ];
+
+    // build rows
+    let rows = [];
+    while (labourTrackerData.hasNext()) {
+        let labourTrackerRecord = labourTrackerData.next();
+        rows.push({
+            position: labourTrackerRecord.position,
+            activeWorkers: labourTrackerRecord.activeWorkersCount + ' <strong>(' + labourTrackerRecord.activeWorkersPercentage + ')</strong>',
+            onBreakWorkers: labourTrackerRecord.onBreakWorkersCount + ' <strong>(' + labourTrackerRecord.onBreakWorkersPercentage + ')</strong>',
+            idleWorkers: labourTrackerRecord.idleWorkersCount + ' <strong>(' + labourTrackerRecord.idleWorkersPercentage + ')</strong>',
+            total: labourTrackerRecord.total
+        });
+    }
+    return {
+        columns: columns,
+        rows: rows
+    };
+}
