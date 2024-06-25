232c9f6b56c1c78862aad3069214ee98a046cb64
smoyano <smoyano@slingr.io>
2024-06-25 12:34:10 -0300

    Act sample summary widget.

--- views/flex/actResultsSummaryTable.js
+++ views/flex/actResultsSummaryTable.js
@@ -10,49 +10,116 @@ function resultsSummaryTableWidgetCalculation(record, options) {
         {
             label: 'Partition ID',
             name: 'partitionId',
-            type: 'html',
+            type: 'text',
             options: {
                 sticky: true
             }
+        },
+        {
+            label: 'Sample name',
+            name: 'sampleName',
+            type: 'text'
+        },
+        {
+            label: 'Sample results',
+            name: 'sampleResults',
+            type: 'html'
+        },
+        {
+            label: 'Position batch',
+            name: 'positionBatch',
+            type: 'text'
+        },
+        {
+            label: 'Customer',
+            name: 'customer',
+            type: 'text'
+        },
+        {
+            label: 'Customer',
+            name: 'customer',
+            type: 'text'
+        },
+        {
+            label: 'Sample matrix',
+            name: 'sampleMatrix',
+            type: 'text'
+        },
+        {
+            label: 'Sample type',
+            name: 'sampleType',
+            type: 'text'
+        },
+        {
+            label: 'Order ID',
+            name: 'orderId',
+            type: 'text'
+        },
+        {
+            label: 'Partition weight',
+            name: 'partitionWeight',
+            type: 'text'
+        },
+        {
+            label: 'Dilution factor',
+            name: 'dilutionFactor',
+            type: 'html'
+        },
+        {
+            label: 'Notes',
+            name: 'notes',
+            type: 'html'
+        },
+        {
+            label: 'Internal notes',
+            name: 'internalNotes',
+            type: 'html'
+        },
+        {
+            label: 'Arsenic',
+            name: 'arsenic',
+            type: 'html'
+        },
+        {
+            label: 'Cadmium',
+            name: 'cadmium',
+            type: 'html'
         }
     ];
-    // build columns dynamically based on data
-    while (resultsData.hasNext()) {
-        let result = resultsData.next();
-        let services = result.analysisServices;
-        for (let serviceIndex = 0; serviceIndex < services.length; serviceIndex++) {
-            let existingColumn = columns.find(column => column.name === services[serviceIndex].name);
-            if (!existingColumn) {
-                columns.push({
-                    label: services[serviceIndex].label,
-                    name: services[serviceIndex].name,
-                    type: 'html'
-                });
-            }
+    let getIconType = function (value) {
+        if (value < 0.5) {
+            return 'check';
         }
-    }
-
+        return 'close';
+    };
+    let getIconColor = function (value) {
+        if (value < 0.5) {
+            return 'success';
+        }
+        return 'default';
+    };
     // build rows
     let rows = [];
     while (resultsData.hasNext()) {
         let result = resultsData.next();
         let partitionId = result.partitionId;
-        let partitionCell = `<slingr-entity-action action="openResults" recordId="${partitionId}">Open results</slingr-entity-action>`;
         let row = {
-            partitionId: partitionCell,
+            partitionId: partitionId,
+            sampleName: result.sampleName,
+            sampleResults: `<slingr-entity-action action="openResults" recordId="${partitionId}">Results</slingr-entity-action>`,
+            positionBatch: result.positionBatch,
+            customer: result.customer,
+            sampleMatrix: result.sampleMatrix,
+            sampleType: result.sampleType,
+            orderId: result.orderId,
+            partitionWeight: result.partitionWeight,
+            dilutionFactor: `${result.dilutionFactor} <slingr-entity-action action="changeDilutionFactor" recordId="${result.id}">Change</slingr-entity-action>`,
+            notes: `${result.notesCount} <slingr-entity-action action="viewNotes" recordId="${result.id}">ViewEdit</slingr-entity-action>`,
+            internalNotes: `${result.internalNotesCount} <slingr-entity-action action="viewInternalNotes" recordId="${result.id}">ViewEdit</slingr-entity-action>`,
+            arsenic: `${result.arsenic} <slingr-icon type="${getIconType(result.arsenic)}" color="${getIconColor(result.arsenic)}"></slingr-icon>`,
+            cadmium: `${result.cadmium} <slingr-icon type="${getIconType(result.cadmium)}" color="${getIconColor(result.cadmium)}"></slingr-icon>`
         };
-        let services = result.analysisServices;
-        for (let serviceIndex = 0; serviceIndex < services.length; serviceIndex++) {
-            let serviceValueCell = {
-                value: services[serviceIndex].raw + '<br>' + services[serviceIndex].result,
-                style: {}
-            };
-            // highlight cell value
-            if (services[serviceIndex].raw > 0.5) {
-                serviceValueCell.style.backgroundColor = 'red';
-            }
-            row[services[serviceIndex].name] = serviceValueCell
-        }
+
         rows.push(row);
     }
     return {
