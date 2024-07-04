46cc2880c40dcb05ad39ca74b6855378de5420d4
smoyano <smoyano@slingr.io>
2024-07-04 16:47:34 -0300

    Refactored act sample

--- views/flex/actSampleSummary.js
+++ views/flex/actSampleSummary.js
@@ -5,52 +5,27 @@ function sampleSummaryTableWidgetCalculation(record, options) {
 
     // build table
 
-    // build columns
-    let columns = [
-        {
-            label: 'Analysis category',
-            name: 'analysisCategory',
-            options: {
-                alignment: 'center'
-            }
-        },
-        {
-            label: 'Status',
-            name: 'status',
-            options: {
-                alignment: 'center'
-            }
-        },
-        {
-            label: 'Done',
-            name: 'done',
-            options: {
-                alignment: 'center'
-            }
-        },
-        {
-            label: 'Verified',
-            name: 'verified',
-            options: {
-                alignment: 'center'
-            }
-        }
-    ];
-
     // build rows
     let rows = [];
     while (analysesData.hasNext()) {
         let analysisRecord = analysesData.next();
         rows.push({
-            analysisCategory:  analysisRecord.category,
-            status:  analysisRecord.status,
-            done: analysisRecord.pending + '/' + analysisRecord.done,
-            verified: analysisRecord.pendingVerification + '/' + analysisRecord.verified,
+            cells: [
+                {headerName: "analysisCategory", value: analysisRecord.field('name').val()},
+                {headerName: "status", value: analysisRecord.field('status').label()},
+                {headerName: "done", value: analysisRecord.pending + '/' + analysisRecord.done},
+                {headerName: "verified", value: analysisRecord.pendingVerification + '/' + analysisRecord.verified}
+            ]
         });
     }
 
     return {
-        columns: columns,
-        rows: rows
+        header: [
+            {name: "analysisCategory", label: "Analysis category", options: {style: {fontWeight: "bold", textAlign: "center"}}},
+            {name: "status", label: "Status", options: {style: {fontWeight: "bold", textAlign: "center"}}},
+            {name: "done", label: "Feb", options: {style: {fontWeight: "bold", textAlign: "center"}}},
+            {name: "verified", label: "Mar", options: {style: {fontWeight: "bold", textAlign: "center"}}},
+        ],
+        body: rows
     };
 }
