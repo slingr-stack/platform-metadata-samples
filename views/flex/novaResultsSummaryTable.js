fbae703533c0bda1daf5aaa0cf3a9131410725e2
smoyano <smoyano@slingr.io>
2024-07-04 16:41:43 -0300

    Refactored nova sample

--- views/flex/novaResultsSummaryTable.js
+++ views/flex/novaResultsSummaryTable.js
@@ -5,58 +5,28 @@ function resultsSummaryTableWidgetCalculation(record, options) {
 
     // build table
 
-    // build columns
-    let columns = [
-        {
-            label: 'Partition ID',
-            name: 'partitionId',
-            type: 'html',
-            options: {
-                sticky: true
-            }
-        }
-    ];
-    // build columns dynamically based on data
-    while (resultsData.hasNext()) {
-        let result = resultsData.next();
-        let services = result.analysisServices;
-        for (let serviceIndex = 0; serviceIndex < services.length; serviceIndex++) {
-            let existingColumn = columns.find(column => column.name === services[serviceIndex].name);
-            if (!existingColumn) {
-                columns.push({
-                    label: services[serviceIndex].label,
-                    name: services[serviceIndex].name,
-                    type: 'html'
-                });
-            }
-        }
-    }
-
     // build rows
     let rows = [];
     while (resultsData.hasNext()) {
         let result = resultsData.next();
         let partitionId = result.partitionId;
-        let partitionCell = `<slingr-entity-action action="openResults" recordId="${partitionId}">Open results</slingr-entity-action>`;
-        let row = {
-            partitionId: partitionCell,
-        };
-        let services = result.analysisServices;
-        for (let serviceIndex = 0; serviceIndex < services.length; serviceIndex++) {
-            let serviceValueCell = {
-                value: services[serviceIndex].raw + '<br>' + services[serviceIndex].result,
-                style: {}
-            };
-            // highlight cell value
-            if (services[serviceIndex].raw > 0.5) {
-                serviceValueCell.style.backgroundColor = 'red';
-            }
-            row[services[serviceIndex].name] = serviceValueCell
-        }
-        rows.push(row);
+        let partitionCell = `<slingr-action action="openResults" recordId="${partitionId}">Open results</slingr-action>`;
+        rows.push({
+            cells: [
+                {headerName: "partitionId", value: partitionCell},
+                {headerName: "dwc242406180013", value: result.sample3.raw + '<br>' + result.sample3.result, options: {style: {backgroundColor: result.sample3.hasIssues() ? 'red' : 'inherit'}}},
+                {headerName: "dwc242406180012", value: result.sample2.raw + '<br>' + result.sample2.result, options: {style: {backgroundColor: result.sample2.hasIssues() ? 'red' : 'inherit'}}},
+                {headerName: "dwc242406180011", value: result.sample1.raw + '<br>' + result.sample1.result, options: {style: {backgroundColor: result.sample1.hasIssues() ? 'red' : 'inherit'}}},
+            ]
+        });
     }
     return {
-        columns: columns,
-        rows: rows
+        header: [
+            {name: "partitionId", label: "Partition ID", options: {style: {fontWeight: "bold"}}},
+            {name: "dwc242406180013", label: "DWC24-240618001-3", options: {style: {fontWeight: "bold"}}},
+            {name: "dwc242406180012", label: "DWC24-240618001-2", options: {style: {fontWeight: "bold"}}},
+            {name: "dwc242406180011", label: "DWC24-240618001-1", options: {style: {fontWeight: "bold"}}}
+        ],
+        body: rows
     };
 }
